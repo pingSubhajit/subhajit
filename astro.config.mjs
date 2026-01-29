0a9bf22e9a1964ff27e533c2b9be3fc1a216f981
Subhajit Kundu <subha60kundu@gmail.com>
2026-01-29 22:06:55 +0530

    feat: add new gallery images & add link prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
-
-  integrations: [react()],
+	integrations: [react()],
+    prefetch: {
+		defaultStrategy: 'viewport',
+		prefetchAll: true
+	}
 });
